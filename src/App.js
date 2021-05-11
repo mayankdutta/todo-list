a7847da88f4bed9ee8decc8cddd38a908d65b21a
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-11 12:34:33 +0530

    useState as an array implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const [list, setList] = useState([""]);
+
+  function takeData(event) {
+    setData(event.target.value);
+  }
+
+  function registerData(event) {
+    event.preventDefault();
+    setList(function () {
+      return [...list, data];
+    });
+  }
+
+  function del(event) {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content-center w-screen h-screen bg-gray-700 grid justify-items-center main">
+      <div className="flex flex-col py-8 bg-white border-4 border-gray-800 shadow-2xl space-y-8 rounded-2xl">
+        <div className="p-4 font-sans text-6xl text-center text-white bg-purple-700 shadow-2xl">
+          ToDo List
+        </div>
+        <form onSubmit={registerData} className="px-4">
+          <input
+            type="text"
+            placeholder="Add a item"
+            name="input"
+            onChange={takeData}
+            value={data}
+            className="p-4 font-sans text-2xl border-b-8 border-transparent focus:border-purple-700 focus:outline-none"
+          />
+          <button
+            type="submit"
+            className="p-4 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-blue-700"
+          >
+            Add
+          </button>
+        </form>
+
+        <div className="text-center">
+          <button
+            onClick={del}
+            className="p-4 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-red-700"
+          >
+            Delete
+          </button>
+
+          <span className="font-sans text-4xl">{list[list.length - 1]}</span>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ temp.jsx
@@ -0,0 +1,15 @@
+
+        <div className="text-center">
+          <button
+            onClick={del}
+            className="p-4 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-red-700"
+          >
+            Delete
+          </button>
+
+          {data.map((d) => {
+            return <span className="font-sans text-4xl"> {d} </span>;
+          })}
+
+          <span className="font-sans text-4xl"> Data</span>
+        </div>
