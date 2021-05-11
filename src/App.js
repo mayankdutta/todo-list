9e5c1707ea6f4842438a70aef07bf563566a74f1
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-11 15:45:50 +0530

    deletion implemented, take care of how to call deletion function u must call from function -> function call type

--- src/App.js
+++ src/App.js
@@ -9,20 +9,30 @@ function App() {
   }
 
   function registerData(event) {
-    event.preventDefault();
+    // event.preventDefault();
     setList(function () {
       return [...list, data];
     });
   }
 
-  function del(event) {}
+  function del(event) {
+    console.log("on pressed id is : " + event);
+    setList(function (prev) {
+      return prev.filter((key, id) => {
+        return id !== event;
+      });
+    });
+  }
   return (
-    <div className="content-center w-screen h-screen bg-gray-700 grid justify-items-center main">
-      <div className="flex flex-col py-8 bg-white border-4 border-gray-800 shadow-2xl space-y-8 rounded-2xl">
+    <div className="content-center bg-gray-400 grid justify-items-center main">
+      <div className="flex flex-col py-8 my-4 bg-white border-4 border-gray-800 shadow-2xl space-y-8 rounded-2xl">
         <div className="p-4 font-sans text-6xl text-center text-white bg-purple-700 shadow-2xl">
           ToDo List
         </div>
-        <form onSubmit={registerData} className="px-4">
+        <div
+          // onSubmit={registerData}
+          className="px-4"
+        >
           <input
             type="text"
             placeholder="Add a item"
@@ -32,25 +42,33 @@ function App() {
             className="p-4 font-sans text-2xl border-b-8 border-transparent focus:border-purple-700 focus:outline-none"
           />
           <button
-            type="submit"
-            className="p-4 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-blue-700"
+            // type="submit"
+            onClick={registerData}
+            className="px-6 py-3 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-blue-700"
           >
             +
           </button>
-        </form>
+        </div>
 
         <div className="text-center">
-          {list.map((d) => {
+          {list.map((d, index) => {
             return (
-              <div className="mx-8 content-center my-2 grid  grid-cols-2 justify-items-center">
+              <div
+                id={index}
+                key={index}
+                className="content-center mx-8 my-2 grid grid-cols-2 justify-items-center"
+              >
                 <button
-                  onClick={del}
-                  className="p-4 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-red-700"
+                  onClick={() => {
+                    del(index);
+                  }}
+                  className="px-6 py-3 text-4xl text-white bg-purple-700 border-8 border-purple-700 rounded-full shadow-2xl hover:border-purple-800 hover:bg-red-700"
                 >
                   X
                 </button>
 
                 <span className="p-4 font-sans text-4xl"> {d} </span>
+                {console.log("Index is : " + index)}
               </div>
             );
           })}
